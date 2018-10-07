c5035097f89fd946239a22db7fa8dc1be6670bb0
Martin Smith <538@holbertonschoo.com>
2018-10-07 19:45:18 +0000

    init

---
+++ 0x08-static_libraries/_src/100-atoi.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * _atoi - the firt whole int number in a string.
+ * @s: string to convert to int
+ *
+ * Return: INT .
+ */
+int _atoi(char *s)
+{
+        return (0);
+}
